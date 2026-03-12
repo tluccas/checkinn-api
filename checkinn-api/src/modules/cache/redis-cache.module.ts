e8b0cb528b73db5d97492f0ac4defdd19dc42876
Lucas Alves <lucas1901alves@gmail.com>
2026-03-11 22:14:15 -0300

    chore(redis): add redis module

---
+++ checkinn-api/src/modules/cache/redis-cache.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get<string>('redis.host'),
+        port: configService.get<number>('redis.port'),
+        ttl: 60 * 10, // TTL padão de 10 min
+      }),
+      isGlobal: true,
+    }),
+  ],
+})
+export class RedisCacheModule {}
