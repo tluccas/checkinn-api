f598e6002d6f9df8d17aee7992b82952439b8611
Lucas Alves <lucas1901alves@gmail.com>
2026-03-12 17:10:25 -0300

    docs(swagger): add swagger initial config

--- checkinn-api/src/main.ts
+++ checkinn-api/src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module.js';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('CheckInn API')
+    .setDescription('API para gerenciamento de reservas de hotel')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
 
   app.setGlobalPrefix('api');
 
@@ -24,6 +35,6 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   logger.log(`CheckInn API rodando na porta ${port}`);
-  logger.log(`Documentação: http://localhost:${port}/api`);
+  logger.log(`Documentação: http://localhost:${port}/api/docs`);
 }
 bootstrap();
